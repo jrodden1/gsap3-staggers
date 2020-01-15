1ce1f42c31abd5f2f423634093da9744ab0e9d31
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-15 11:03:14 -0700

    Updated notes about Advanced Stagger

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // added stagger
 // gsap.to("#freds img", {y:-50, stagger: 0.1});  BASIC STAGGER
 
-// Advanced Stagger
+// Advanced Stagger - stagger can take an object
 gsap.to("#freds img", {y:-50, stagger: {
    each: 0.02, // each is essentially the same thing as line 5 - separate the staggering in by .2 secs.
    // amount: 1 //amount of time to pass that the stagger needs to move all the objects in by (in this case all 6 freds need to start being animated in before 1 second is up)
